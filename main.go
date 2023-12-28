2aa6945bb4efcb44fc0b11936406b498520f4f5f
Arthur Nicholas <arthurn0606@gmail.com>
2023-12-28 11:03:57 -0300

    FT: setting config

--- main.go
+++ main.go
@@ -1,8 +1,19 @@
 package main
 
-import "github.com/arthurnich0las/go_opportunities/router"
+import (
+	"github.com/arthurnich0las/go_opportunities/config"
+	"github.com/arthurnich0las/go_opportunities/router"
+)
 
 func main() {
+	logger := config.GetLogger("main")
+	// Inicializing configs
+	err := config.Init()
+	if err != nil {
+		logger.Errorf("config initialization error: %v", err)
+		return
+	}
+
 	// Initializing router
 	router.Initialize()
 }
